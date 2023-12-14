4c476966f63afc249d18f0361ba6dc000a7b0d75
Vineeth Chandran <vineeth@exdera.com>
2023-12-14 15:40:58 +0530

    flavor configured

---
+++ lib/fin_flow_app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/dependancy_injection/config/service_locator.dart';
+import 'core/theme/app_theme.dart';
+import 'features/authentication/presentation/authentication_bloc/authentication_bloc.dart';
+import 'features/home/presentation/bloc_and_cubits/add_transaction_sheet_cubit/add_transaction_sheet_cubit.dart';
+import 'features/home/presentation/bloc_and_cubits/home_screen_bloc/home_screen_bloc.dart';
+import 'features/splash/presentation/cubit/splash_screen_cubit.dart';
+import 'features/splash/presentation/pages/splash_screen.dart';
+
+class FinFlowApp extends StatelessWidget {
+  const FinFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<AddTransactionSheetCubit>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<AuthenticationBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<HomeScreenBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => SplashScreenCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: AppTheme.theme,
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
